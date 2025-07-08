e9dd883805e9860cb79bbfea7e5d62f90514d1ea
Travis1212321 <rekardo647@gmail.com>
2025-07-08 17:08:26 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ const chalk = require('chalk')
 require('dotenv').config()
 
 //contact details - sensitive info moved to .env
-global.SESSION_ID = process.env.SESSION_ID || "HansTz&W1QwGDhC#dNjvrh6Ka1vMksE2QUz4isFlFeWKfp24NdHfsmg5gI8" //fallback to default if not in .env
+global.SESSION_ID = process.env.SESSION_ID || "HansTz&2xoWGYSB#gCrfjyWwCbeF-GuXeLLYhORoBiC_1vdemhGtgRhfB6A" //fallback to default if not in .env
 global.ownernomer = process.env.OWNER_NUMBER || "249126083647"
 
 global.ownername = process.env.OWNER_NAME || "otto²"
